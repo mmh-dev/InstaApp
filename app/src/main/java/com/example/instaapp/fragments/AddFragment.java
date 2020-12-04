e2211d54d6e06857d94980e79be8769a60866543
mmh-dev <murod.hodjaev@gmail.com>
2020-12-04 08:22:34 +0600

    1

--- app/src/main/java/com/example/instaapp/fragments/AddFragment.java
+++ app/src/main/java/com/example/instaapp/fragments/AddFragment.java
@@ -3,6 +3,7 @@ package com.example.instaapp.fragments;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -19,10 +20,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.instaapp.PhotoAdapter;
 import com.example.instaapp.R;
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -51,7 +62,6 @@ public class AddFragment extends Fragment {
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
         adapter = new PhotoAdapter(photosList, getContext());
 
-
         uploadPhotoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,11 +89,65 @@ public class AddFragment extends Fragment {
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
                 photosList.add(bitmap);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                ParseFile parseFile = new ParseFile("image.png", byteArray);
+                parseFile.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(getActivity(), "Image is saved!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                ParseObject object = new ParseObject("Photo");
+                object.put("title", "Picture 1");
+                object.put("image", parseFile);
+                object.put("userId", ParseUser.getCurrentUser().getUsername());
+                object.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(getActivity(), "Object is saved!", Toast.LENGTH_SHORT).show();
+//                            retrieveObject();
+                        } else {
+                            Log.i("error", ""+ e.getMessage());
+                        }
+                    }
+                });
             } catch (IOException e) {
                 Log.i("error", "" + e.getMessage());
             }
-            recyclerView.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
-            }
         }
+    }
+
+    private void retrieveObject() {
+        ParseQuery<ParseObject> query  = new ParseQuery<ParseObject>("Photo");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                for (ParseObject object: objects) {
+                    ParseFile file = (ParseFile) object.get("image");
+                    file.getDataInBackground(new GetDataCallback() {
+                        @Override
+                        public void done(byte[] data, ParseException e) {
+                            if(e == null && data != null){
+                                Bitmap bitmap = BitmapFactory.decodeByteArray(data,0,data.length);
+                            } else {
+                                Toast.makeText(getContext(), "Error in downloading a data!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void uploadObject(ParseFile parseFile) {
+
+    }
 }
